664b069bb6e7a32e39d952092487c68e764e5140
kyleuniversities <kyleuniversities@gmail.com>
2023-10-22 18:12:53 -0700

    allowed for json test output

--- src/main/java/com/ku/quizzical/TestController.java
+++ src/main/java/com/ku/quizzical/TestController.java
@@ -1,13 +1,15 @@
 package com.ku.quizzical;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public final class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Test";
+        return "\"Test\"";
     }
 }
