ab2544ca298b2af75563ad0cd19334a2ba6d6669
kyleuniversities <kyleuniversities@gmail.com>
2023-11-17 02:25:15 -0800

    removed jdbc config

--- src/main/java/com/ku/quizzical/app/config/WebSecurityConfiguration.java
+++ src/main/java/com/ku/quizzical/app/config/WebSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package com.ku.quizzical.app.config;
 
 import java.util.Arrays;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
